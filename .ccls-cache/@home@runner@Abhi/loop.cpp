f8eb587e1c68a50de746e29d35a4f6b0ae21c7ae
AbhinavAnand098 <160204055+AbhinavAnand098@users.noreply.github.com>
2024-03-06 16:38:32 +0000

    loops added

---
+++ .ccls-cache/@home@runner@Abhi/loop.cpp
@@ -0,0 +1,8 @@
+#include <iostream>
+using namespace std;
+
+void loop() {
+  for (int i = 0; i < 10; i++) {
+    cout << i << " ";
+  }
+}
--- main.cpp
+++ main.cpp
@@ -1,29 +1,30 @@
 #include <iostream>
 using namespace std;
 
+void loop();
 int main() {
 
-  cout << "enter rows";
-  int n;
+  // cout << "enter rows";
+  // int n;
 
-  cin >> n;
-  int i = 1;
-  while (i <= n) {
-    int space = n - i;
-    while (space) {
-      cout << " ";
-      space--;
-    }
-    int j = 1, k = 1;
-    while (j <= i) {
-      cout << "*";
-      j++;
-    }
-    while (k <= i) {
-      cout << "*";
-      k++;
-    }
-    cout << endl;
-    i++;
-  }
+  // cin >> n;
+  // int i = 1;
+  // while (i <= n) {
+  //   int space = n - i;
+  //   while (space) {
+  //     cout << " ";
+  //     space--;
+  //   }
+  //   int j = 1, k = 1;
+  //   while (j <= i) {
+  //     cout << "*";
+  //     j++;
+  //   }
+  //   while (k <= i) {
+  //     cout << "*";
+  //     k++;
+  //   }
+  //   cout << endl;
+  //   i++;
+  // }
 }
